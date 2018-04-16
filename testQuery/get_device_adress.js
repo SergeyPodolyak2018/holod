72ff699f54147aad1e46f59a480e4a0330661969
SergeyPodolyak2018 < sergey.podolyak2018@gmail.com>
2018-04-16 16:51:19 +0300

    fix sensors in index

--- testQuery/get_device_adress.js
+++ testQuery/get_device_adress.js
@@ -13,9 +13,15 @@ const getDeviceAdress=(data)=>{
 			'aCamera':0,
 			'aInletValve':0,
 			'aHeatingPipes':0,			
-			'aAirCooler':0
+			'aAirCooler':0,
+			'aComp':0,
+			'aVessel':0,
+			'aPumpsGroup':0,
+			'aReceiver':0,
+			'aCapasitor':0
+
 		}
-		let re1=/([A-Z])/g;
+		let re1=/([a-zA-Z]{1,})/g;
 		let re2=/(\d)/g;
 		
 		for(let i in splitName){			
@@ -23,21 +29,47 @@ const getDeviceAdress=(data)=>{
 			let reIndex=parseInt(splitName[i].match(re2)[0]);
 			if(reRezName=='F'){
 				wayToTarget.aFloor=reIndex;
+				continue;
 			}
 			if(reRezName=='H' & i<2){
 				wayToTarget.aHeatingLounger=reIndex;
+				continue;
 			}
 			if(reRezName=='B'){
 				wayToTarget.aCamera=reIndex;
+				continue;
 			}
 			if(reRezName=='H' & i>1){
 				wayToTarget.aHeatingPipes=reIndex;
+				continue;
 			}
 			if(reRezName=='G'){
 				wayToTarget.aInletValve=reIndex;
+				continue;
 			}
 			if(reRezName=='C'){
 				wayToTarget.aAirCooler=reIndex;
+				continue;
+			}
+			if(reRezName=='S'){
+				wayToTarget.aComp=reIndex;
+				continue;
+			}
+			if(reRezName=='R'){
+				wayToTarget.aVessel=reIndex;
+				continue;
+			}
+			if(reRezName=='P'){
+				wayToTarget.aPumpsGroup=reIndex;
+				continue;
+			}
+			if(reRezName=='J'){
+				wayToTarget.aReceiver=reIndex;
+				continue;
+			}
+			if(reRezName=='Rf'){
+				wayToTarget.aCapasitor=reIndex;
+				continue;
 			}
 		}
 		
