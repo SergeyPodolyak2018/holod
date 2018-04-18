2b53cad2bee5f39dce2255aaad7508d4c3ae469e
SergeyPodolyak2018 < sergey.podolyak2018@gmail.com>
2018-04-18 16:30:57 +0300

    Start make settings

--- testQuery/get_device_adress.js
+++ testQuery/get_device_adress.js
@@ -7,7 +7,6 @@ const getDeviceAdress=(data)=>{
 			'NameShort':'',
 			'EIndex':0,
 			'EType':0,
-			'EFlags':0,
 			'aFloor':0,
 			'aHeatingLounger':0,
 			'aCamera':0,
@@ -31,57 +30,64 @@ const getDeviceAdress=(data)=>{
 				wayToTarget.aFloor=reIndex;
 				continue;
 			}
-			if(reRezName=='H' & i<2){
+			if(reRezName=='H' && i<2){
 				wayToTarget.aHeatingLounger=reIndex;
+				wayToTarget.EType=2;
 				continue;
 			}
 			if(reRezName=='B'){
 				wayToTarget.aCamera=reIndex;
+				if (i<2){
+					wayToTarget.EType=4;
+				}
 				continue;
 			}
-			if(reRezName=='H' & i>1){
+			if(reRezName=='H' && i>1){
 				wayToTarget.aHeatingPipes=reIndex;
+				wayToTarget.EType=2;
 				continue;
 			}
 			if(reRezName=='G'){
 				wayToTarget.aInletValve=reIndex;
+				wayToTarget.EType=3;
 				continue;
 			}
 			if(reRezName=='C'){
 				wayToTarget.aAirCooler=reIndex;
+				wayToTarget.EType=1;
 				continue;
 			}
 			if(reRezName=='S'){
 				wayToTarget.aComp=reIndex;
+				wayToTarget.EType=5;
 				continue;
 			}
 			if(reRezName=='R'){
 				wayToTarget.aVessel=reIndex;
+				wayToTarget.EType=6;
 				continue;
 			}
 			if(reRezName=='P'){
 				wayToTarget.aPumpsGroup=reIndex;
+				wayToTarget.EType=7;
 				continue;
 			}
 			if(reRezName=='J'){
 				wayToTarget.aReceiver=reIndex;
+				wayToTarget.EType=8;
 				continue;
 			}
 			if(reRezName=='Rf'){
 				wayToTarget.aCapasitor=reIndex;
+				wayToTarget.EType=9;
 				continue;
 			}
 		}
 		
 		wayToTarget.NameLong=data.NameLong;
 		wayToTarget.NameShort=data.NameShort;		
-		wayToTarget.EType=parseInt(data.EType);
-
-
 
 		return wayToTarget;
 }
-
-
 
 exports.getDeviceAdress=getDeviceAdress;
